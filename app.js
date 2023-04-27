82245f2d30d5ea2ab346cfb1cf9aeb165f919ec2
Miracle Jude <judemiraco0@gmail.com>
2023-04-27 22:58:50 +0100

    update app.js

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ hidePasswordIcon.addEventListener('click', function() {
 
 
 // form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   // Display the 404 page
-//   window.location.href = '404.html';
+  // event.preventDefault();
+  // Display the 404 page
+  // window.location.href = '404.html';
 
-//   // Store the email and password in local storage
-//   // const email = document.getElementById('username').value;
-//   // const password = document.getElementById('password').value;
-//   // localStorage.setItem('email', email);
-//   // localStorage.setItem('password', password);
+  // Store the email and password in local storage
+  // const email = document.getElementById('username').value;
+  // const password = document.getElementById('password').value;
+  // localStorage.setItem('email', email);
+  // localStorage.setItem('password', password);
 // });
